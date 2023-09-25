630498eac7a093571aab495b4c6a095cadaa4a30
sylvan-li <76680992+sylvan-li@users.noreply.github.com>
2023-09-25 05:39:51 -0700

    add numberline file

---
+++ numberline.js
@@ -0,0 +1,23 @@
+//Number Line
+//ask user for input for both variables
+//initialize these variables 
+//initialize the sum of two inputs as a variable 
+//write parameters that meet expected print
+// write expected printout + user inputed sum
+
+let num1=-5;
+let num2=0;
+let sum=(num1+num2);
+
+if(num1+num2<0){
+    console.log(sum+" is a negative number");
+}else{
+    if(num1+num2>100){
+        console.log(sum + " is greater than 100");
+    }else{
+        if(num1+num2>0){
+            console.log(sum + " is greater than 0");
+        }
+    }{if(num1+num2===0){console.log(sum + " is equal to 0");}}
+}
+
